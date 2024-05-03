31209e99d9be0d7a66229c6df87a06541dfa7828
Rudrakumar Patel <rudrapatel2992003@gmail.com>
2024-05-03 09:01:35 +0530

    update

--- COMMENT_ANALYZER_APP/Front_end/src/components/Searchbar.jsx
+++ COMMENT_ANALYZER_APP/Front_end/src/components/Searchbar.jsx
@@ -88,63 +88,6 @@ export const Searchbar = () => {
       setFetching(false);
       console.error("There was a problem with your fetch operation:", error);
     }
-
-    // if (displayButton) {
-    //   // node --version # Should be >= 18
-    //   // npm install @google/generative-ai
-
-    //   const {
-    //     GoogleGenerativeAI,
-    //     HarmCategory,
-    //     HarmBlockThreshold,
-    //   } = require("@google/generative-ai");
-
-    //   const MODEL_NAME = "gemini-1.5-pro-latest";
-    //   const API_KEY = "AIzaSyCfKuk2qFxV_VKlnwrO72T2JBG063fJLKQ";
-
-    //   async function runChat() {
-    //     const genAI = new GoogleGenerativeAI(API_KEY);
-    //     const model = genAI.getGenerativeModel({ model: MODEL_NAME });
-
-    //     const generationConfig = {
-    //       temperature: 1,
-    //       topK: 0,
-    //       topP: 0.95,
-    //       maxOutputTokens: 8192,
-    //     };
-
-    //     const safetySettings = [
-    //       {
-    //         category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-    //         threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    //       },
-    //       {
-    //         category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    //         threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    //       },
-    //       {
-    //         category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-    //         threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    //       },
-    //       {
-    //         category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    //         threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    //       },
-    //     ];
-
-    //     const chat = model.startChat({
-    //       generationConfig,
-    //       safetySettings,
-    //       history: [],
-    //     });
-
-    //     const result = await chat.sendMessage("hi");
-    //     const response = result.response;
-    //     console.log(response.text());
-    //   }
-
-    //   runChat();
-    // }
   };
 
   return (
